1492d35cc9b05b144d1b648e08d6d68018521ec2
ahmedbasha10 <62184511+ahmedbasha10@users.noreply.github.com>
2021-09-23 18:35:13 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -10,7 +10,7 @@ const contentDiv = document.getElementById('content');
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = (day.getMonth() + 1) + '/'+ day.getDate()+'/'+ day.getFullYear();
 
 generateButton.addEventListener('click', generateData);
 
